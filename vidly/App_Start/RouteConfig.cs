42e058d930557335ba38cfec076e0bdc3984d8ce
pierluigi <pierlucolombo@gmail.com>
2021-02-12 16:14:39 +0100

    add route

--- vidly/App_Start/RouteConfig.cs
+++ vidly/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace vidly
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "MoviesByReleaseDate",
+                url: "movies/released/{year}/{month}",
+                defaults: new { controller = "Movies", action = "ByReleaseDate" }
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
