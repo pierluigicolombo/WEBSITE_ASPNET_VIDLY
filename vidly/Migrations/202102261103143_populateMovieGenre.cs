39f2cb5d05192e37d4ef599ef17995686bed280d
pierluigi <pierlucolombo@gmail.com>
2021-02-26 12:37:25 +0100

    popolated MovieGenre Table

---
+++ vidly/Migrations/202102261103143_populateMovieGenre.cs
@@ -0,0 +1,19 @@
+namespace vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class populateMovieGenre : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO MovieGenres (Name) VALUES ('Family')");
+            Sql("INSERT INTO MovieGenres (Name) VALUES ('Action')");
+            Sql("INSERT INTO MovieGenres (Name) VALUES ('Comedy')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
