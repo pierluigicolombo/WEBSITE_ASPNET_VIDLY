5300c5ca858aa9389e505e7f1bca2c8a35eef79c
pierluigi <pierlucolombo@gmail.com>
2021-02-26 09:56:03 +0100

    add birthday date to customer schema

---
+++ vidly/Migrations/202102260853348_addBirthdayToCustomer.cs
@@ -0,0 +1,18 @@
+namespace vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addBirthdayToCustomer : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "Birthdate", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "Birthdate");
+        }
+    }
+}
--- vidly/Models/Customer.cs
+++ vidly/Models/Customer.cs
@@ -11,13 +11,19 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
         public bool IsSubScribedToNewsletter { get; set; }
+
         public MembershipType MembershipType { get; set; }
+
         [ForeignKey("MembershipType")]
         public byte MembershipTypeId { get; set; } 
+
+        public string Birthdate { get; set; }
 
     }
 }
